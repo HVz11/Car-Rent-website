7df391d6bd0d8a276d3d7f6d2549992e37006580
Vaibhav <vs630916@gmail.com>
2023-08-13 16:06:17 +0530

    Topsection of our website

--- src/app/containers/Homepage/index.tsx
+++ src/app/containers/Homepage/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { styled } from "styled-components";
 import tw from "twin.macro";
 import { Navbar } from "../../components/navbar";
+import { TopSection } from "./topSection";
 
 const PageContainer = styled.div`
 ${tw`
@@ -17,5 +18,6 @@ overflow-x-hidden
 export function HomePage() {
     return <PageContainer>
         <Navbar />
+        <TopSection />
     </PageContainer>
 }
