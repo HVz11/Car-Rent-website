28ca597316d5dba00a4aaa235fd027161cc433ea
Vaibhav <vs630916@gmail.com>
2023-08-13 23:42:23 +0530

    BookingCard Component

--- src/app/containers/Homepage/index.tsx
+++ src/app/containers/Homepage/index.tsx
@@ -3,6 +3,8 @@ import { styled } from "styled-components";
 import tw from "twin.macro";
 import { Navbar } from "../../components/navbar";
 import { TopSection } from "./topSection";
+import { Marginer } from "../../components/marginer";
+import { BookCard } from "../../components/BookCard";
 
 const PageContainer = styled.div`
 ${tw`
@@ -19,5 +21,7 @@ export function HomePage() {
     return <PageContainer>
         <Navbar />
         <TopSection />
+        <Marginer direction="vertical" margin="4em" />
+        <BookCard />
     </PageContainer>
 }
